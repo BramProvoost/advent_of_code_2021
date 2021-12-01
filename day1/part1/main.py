8d969e69ab99ff8026ab47c61b44e0c1a8477107
Bram <bramprovoost@gmail.com>
2021-12-01 10:28:21 +0100

    shorter

--- day1/part1/main.py
+++ day1/part1/main.py
@@ -2,9 +2,8 @@ text_file = open("data.txt", "r")
 lines = text_file.read().split('\n')
 increased = 0
 
-for i in range(len(lines)):
-	if (i > 0):
-		if (int(lines[i]) > int(lines[i - 1])):
-			increased += 1
+for i in range(1, len(lines)):
+	if (int(lines[i]) > int(lines[i - 1])):
+		increased += 1
 
 print("increased: {}".format(increased))
